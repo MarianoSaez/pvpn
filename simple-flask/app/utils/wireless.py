249be4c9f3cf4cb97bafc4be0d9f1ff025f28702
MarianoSaez <mm.saez@alumno.um.edu.ar>
2023-06-09 11:14:36 -0300

    Dashboard - Priv. & Pub. WLAN. Se agrego infromacion de la redes inalambricas al dashboard.

--- simple-flask/app/utils/wireless.py
+++ simple-flask/app/utils/wireless.py
@@ -1,5 +1,6 @@
 from subprocess import run
 from re import findall, search, MULTILINE
+from json import loads
 
 
 
@@ -23,3 +24,44 @@ def scan() -> list[WLAN]:
             wlan_list.append(WLAN(ssid.group(1).strip(), security.group(0).strip(), strength.group(1).strip()))
 
     return wlan_list
+
+def getPubWLANDetails() -> dict:
+    status = {}
+    output = run("wifi status", shell=True, capture_output=True, text=True).stdout
+    data = loads(output)
+    # Obtener estado referido a la red publica
+    if data["radio1"]["up"] == False and data["radio1"]["pending"] == False:
+        status["pubwlanstatus"] = "Disabled"
+    elif data["radio1"]["up"] == False and data["radio1"]["pending"] == True:
+        status["pubwlanstatus"] = "Pending"
+    else:
+        status["pubwlanstatus"] = "Enabled"
+
+    # Obtener nombre de la red
+    wlan_config = data["radio1"]["interfaces"][0]["config"]
+    status["pubwlanssid"] = wlan_config["ssid"]
+
+    return status
+
+def getPrivWLANSDetails() -> dict:
+    status = {}
+    output = run("wifi status", shell=True, capture_output=True, text=True).stdout
+    data = loads(output)
+    # Obtener estado referido a la red publica
+    if data["radio0"]["up"] == False and data["radio0"]["pending"] == False:
+        status["privwlanstatus"] = "Disabled"
+    elif data["radio0"]["up"] == False and data["radio0"]["pending"] == True:
+        status["privwlanstatus"] = "Pending"
+    else:
+        status["privwlanstatus"] = "Enabled"
+
+    # Obtener nombre de la red
+    wlan_config = data["radio0"]["interfaces"][0]["config"]
+    status["privwlanssid"] = wlan_config["ssid"]
+
+    output = run("iwinfo wlan0 assoclist", shell=True, capture_output=True, text=True).stdout
+    clients = findall(r"(?:[0-9A-F]{2}:){5}[0-9A-F]{2}.*", output)
+    status["clients"] = clients
+
+
+    return status
